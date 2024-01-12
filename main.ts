a71d65ac18633338018f195192ab3723d3fc8ded
gbantique <gbantique@gmail.com>
2024-01-12 16:36:39 +0800

    modified test file to use class

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 let ht16k33 = Brickcell.create();
-ht16k33.setAddress(112);
+ht16k33.setAddress(0x70);
 let smile = [
 60,
 66,
@@ -9,11 +9,16 @@ let smile = [
 153,
 66,
 60
-]
+];
+let frown = [60, 66, 165, 129, 153, 165, 66, 60];
 
-Brickcell.setBrightness(15);
-Brickcell.render(smile);
+
 
 basic.forever(function () {
-
+    ht16k33.setBrightness(15);
+    ht16k33.render(smile);
+    basic.pause(1000);
+    ht16k33.setBrightness(5);
+    ht16k33.render(frown);
+    basic.pause(1000);
 })
