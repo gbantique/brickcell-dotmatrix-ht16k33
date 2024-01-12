782161a13d8fbc33b34f8071cadd606f529501c7
gbantique <gbantique@gmail.com>
2024-01-12 13:29:28 +0800

    Update main.blocks, main.ts, brickcell-dotmatrix-ht16k33.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,18 @@
-// tests go here; this will not be compiled when this package is used as a library
-serial.setBaudRate(9600);
-HT16K33_Alnum4.init();
-HT16K33_Alnum4.set_blink_rate(blinkFrequency.off);
-HT16K33_Alnum4.set_brightness(15);
-basic.forever(() => {
-    HT16K33_Alnum4.showString("Hello, World!");
-    HT16K33_Alnum4.showNumber(-1.23456);
-    basic.pause(5000);
+let frown: number[] = []
+let meh: number[] = []
+let smile: number[] = []
+ht16k33.setAddress(HT16K33_I2C_ADDRESSES.ADD_0x70)
+smile = [60, 66, 165, 129, 165, 153, 66, 60]
+meh = [60, 66, 165, 129, 189, 129, 66, 60]
+frown = [60, 66, 165, 129, 153, 165, 66, 60]
+forever(function () {
+    ht16k33.setBrightness(0)
+    ht16k33.render(frown)
+    pause(1000)
+    ht16k33.setBrightness(7)
+    ht16k33.render(meh)
+    pause(1000)
+    ht16k33.setBrightness(15)
+    ht16k33.render(smile)
+    pause(1000)
 })
